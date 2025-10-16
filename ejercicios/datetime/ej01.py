64b15e0b9d64d1bd0dcea936af4dc7df33016c18
emmanuelbg10 <emmanuelbgiraldo@gmail.com>
2025-10-16 17:45:13 +0100

    iniciando datetime

---
+++ ejercicios/datetime/ej01.py
@@ -0,0 +1,7 @@
+import datetime
+
+
+fecha_hora_actual = datetime.datetime.now()
+
+
+print("Fecha y hora actual:", fecha_hora_actual)
---
+++ ejercicios/datetime/ej02.py
@@ -0,0 +1,2 @@
+from datetime import date
+print(date(2005, 2, 10).isoformat())
